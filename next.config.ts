4b33957eb1f8718071bf5086cb9caa05747f4d27
codebiruni <codebiruny@gmail.com>
2026-05-29 22:44:17 +0600

    fixed issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Directs webpack to ignore handlebars' internal runtime extensions feature
+      config.externals.push({
+        handlebars: 'commonjs handlebars',
+      });
+    }
+    return config;
+  },
+  
   images: {
     remotePatterns: [
       {
